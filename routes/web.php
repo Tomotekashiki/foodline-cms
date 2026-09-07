e4c701622c0ac3301e4b32966ccfb030b6dbb1ed
Nodar <nodo@connect.ge>
2026-09-07 11:01:12 +0400

    Add /debug-env route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/debug-env', function () {
+    $sourceDb = base_path('database/database.sqlite');
+    $targetDb = '/tmp/database.sqlite';
+    return response()->json([
+        'base_path' => base_path(),
+        'source_exists' => file_exists($sourceDb),
+        'source_size' => file_exists($sourceDb) ? filesize($sourceDb) : null,
+        'target_exists' => file_exists($targetDb),
+        'target_size' => file_exists($targetDb) ? filesize($targetDb) : null,
+        'db_config' => config('database.connections.sqlite'),
+        'dir_database' => is_dir(base_path('database')) ? scandir(base_path('database')) : null,
+    ]);
+});
+
