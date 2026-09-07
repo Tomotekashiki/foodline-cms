eee9b6d43b8a1a2d7d38554c95b972ddc398abb2
Nodar <nodo@connect.ge>
2026-09-07 11:47:45 +0400

    Add hashing test to debug-env

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,23 @@ Route::get('/debug-env', function () {
         'target_size' => file_exists($targetDb) ? filesize($targetDb) : null,
         'db_config' => config('database.connections.sqlite'),
         'dir_database' => is_dir(base_path('database')) ? scandir(base_path('database')) : null,
+        'bcrypt_defined' => defined('PASSWORD_BCRYPT'),
+        'argon2i_defined' => defined('PASSWORD_ARGON2I'),
+        'argon2id_defined' => defined('PASSWORD_ARGON2ID'),
+        'test_bcrypt' => (function() {
+            try {
+                return password_hash('secret', PASSWORD_BCRYPT);
+            } catch (\Throwable $e) {
+                return 'Error: ' . $e->getMessage();
+            }
+        })(),
+        'test_hash_make' => (function() {
+            try {
+                return \Illuminate\Support\Facades\Hash::make('secret');
+            } catch (\Throwable $e) {
+                return 'Error: ' . $e->getMessage() . ' (' . get_class($e) . ') in ' . $e->getFile() . ':' . $e->getLine();
+            }
+        })(),
     ]);
 });
 
